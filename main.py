44bae27f82dd7a6bdd711efa44f289378d3a3155
Phạm Quốc Huy <40145089+PhamQuocHuy1101@users.noreply.github.com>
2022-04-23 10:45:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,19 @@ def find_parameters(in_size, out_size, n_layer, layer = 'conv', s = [1, 2], k = 
       return s_c, k_c.data
     count += 1
   return None
+
+import os
+from omegaconf import OmegaConf
+
+def load_seq_config(config_dir, config_name):
+    cf = OmegaConf.load(os.path.join(config_dir, config_name  +'.yaml'))
+    defaults = []
+    sub_module = cf.get('defaults', [])
+    for sm in sub_module:
+        name, sub = sm.items()[0]
+        cf_sub_module = load_seq_config(os.path.join(config_dir, name), sub)
+        cf_item = OmegaConf.create({})
+        OmegaConf.update(cf_item, name, cf_sub_module)
+        defaults.append(cf_item)
+    cf = OmegaConf.merge(cf.copy(), *defaults)
+    return cf
